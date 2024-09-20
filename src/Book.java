50842b5b144fe872db0d1cbd18902d0940f122cb
Olga <olganeboga209@gmail.com>
2024-09-20 16:45:54 +0600

    First commit

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    private String title;
+    private String author;
+    private long isbn;
+    private int price;
+
+    public Book(String title, String author, long isbn, int price) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+}
